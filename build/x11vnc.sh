234660524fe5d1d7774c5684cacbae36ba3cbfd9
Carsten Perthel <carsten@cpesoft.com>
2017-10-22 15:52:24 +0200

    Start zello during startup

--- build/x11vnc.sh
+++ build/x11vnc.sh
@@ -1,7 +1,11 @@
 #! /bin/sh
 
+cd ~
 Xvfb :99 -ac -listen tcp -screen 0 800x600x24 &
 sleep 3
 /usr/bin/fluxbox -display :99 -screen 0 &
 sleep 3
 x11vnc -display :99.0 -forever -passwd ${X11VNC_PASSWORD:-password}
+~/zello/zello &
+
+sleep 3
