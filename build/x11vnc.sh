81e0b95693e9a274786e41c0c80acc23f5089bc1
Carsten Perthel <carsten@cpesoft.com>
2017-10-22 15:53:44 +0200

    Optimize x11vnc settings

--- build/x11vnc.sh
+++ build/x11vnc.sh
@@ -5,7 +5,8 @@ Xvfb :99 -ac -listen tcp -screen 0 800x600x24 &
 sleep 3
 /usr/bin/fluxbox -display :99 -screen 0 &
 sleep 3
-x11vnc -display :99.0 -forever -passwd ${X11VNC_PASSWORD:-password}
 ~/zello/zello &
 
 sleep 3
+x11vnc -display :99.0 -forever -passwd ${X11VNC_PASSWORD:-password} -xkb -noxrecord -noxfixes -noxdamage -wait 5 -shared
+
